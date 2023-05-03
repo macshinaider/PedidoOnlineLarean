dd65509b46946d0cf52d40cf6330a29651e664c8
Lucas Santana <lucasadd15@gmail.com>
2023-05-02 21:31:07 -0300

    atualização

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/Main'
 import BurgersPage from './pages/Main/Burgers'
 import PizzasPage from './pages/Main/Pizzas'
+import BebidasPage from './pages/Main/Bebidas'
+import SobremesasPage from './pages/Main/Sobremesas'
 
 export function AppRoutes() {
   return (
@@ -10,6 +12,8 @@ export function AppRoutes() {
       <Route path='/' element={<MainPage />}>
         <Route path='/' element={<BurgersPage />} />
         <Route path='pizzas' element={<PizzasPage />} />
+        <Route path='bebidas' element={<BebidasPage />} />
+        <Route path='sobremesas' element={<SobremesasPage />} />
       </Route>
     </Routes>
   )
